c82142fb0aca3ed167b53e302150effd50ecdeed
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-28 14:49:48 +1000

    Minifier

---
+++ README.md
@@ -0,0 +1,14 @@
+# JavaScript Helper files for Splunk
+
+
+## Future
+
+Use rollup or something to bundle the files
+
+# License
+
+Copyright © 2020 Andrew Wong
+
+This software is licensed under the MIT License. You are free to redistribute it and/or modify it under the terms of the license.
+
+For more details see the license file
--- helper.js
+++ helper.js
@@ -1,11 +1,12 @@
-const modules = [
-    'Collections',
-    'Search',
-    'DOM'
-];
+;(function () {
+  const modules = ['Collections', 'Search', 'DOM']
 
-/* Load all the modules */
-// returns a {name: module, ...} map
-define(modules.map(module => `./lib/${module}.js`), function () {
-    return modules.reduce((obj, key, idx) => ({ ...obj, [key]: arguments[idx] }), {})
-})
+  /* Load all the modules */
+  // returns a {name: module, ...} map
+  define(modules.map(module => `./lib/${module}.js`), function () {
+    return modules.reduce(
+      (obj, key, idx) => ({ ...obj, [key]: arguments[idx] }),
+      {}
+    )
+  })
+})()
